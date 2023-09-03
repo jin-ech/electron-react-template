7be7b24ed502a26d6205184f30974119f0e968ca
JC96821 <13478707150@163.com>
2023-09-03 21:58:32 +0800

    [feature] add background texture

--- src/components/backend-model/index.jsx
+++ src/components/backend-model/index.jsx
@@ -2,36 +2,41 @@
  * @Author: JC96821 13478707150@163.com
  * @Date: 2023-09-02 21:47:27
  * @LastEditors: JC96821 13478707150@163.com
- * @LastEditTime: 2023-09-03 21:46:33
+ * @LastEditTime: 2023-09-03 21:57:18
  * @FilePath: \electron-react-template\src\pages\dashboard\Model.jsx
  * @Description: 背景模型
  */
 
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 import useLoaderUrl from '@/hooks/useLoaderUrl';
 import FallbackLoading from '../fallback-loading';
 
-const targetPosition = [0, -3, -3];
+const targetPosition = [0, 0, 0];
 
-function Model({
+const Model = ({
     url
-}) {
+}) => {
     const gltf = useLoader(GLTFLoader, url);
+    const groupRef = useRef();
+
+    useFrame(() => {
+        groupRef.current.rotation.y += 0.001;
+    });
 
     return (
-        <group position={targetPosition}>
+        <group ref={groupRef} position={targetPosition}>
             <primitive object={gltf.scene} />
         </group>
     );
 }
 
-const staticPath = '/static/models/real-time_light_demo_rotate_lighting_alt-drag/scene.gltf';
-function BackModel() {
+const staticPath = '/static/models/free_-_skybox_cliffside/scene.gltf';
+const BackModel = () => {
     const { url } = useLoaderUrl(staticPath);
 
     if (!url) {
@@ -42,20 +47,15 @@ function BackModel() {
         <Suspense fallback={<FallbackLoading />}>
             <Canvas style={{ width: '100vw', height: '100vh' }}>
                 <ambientLight intensity={4} color="#fff" />
-                <pointLight intensity={8} color="#fff" position={[0, 20, -20]} />
-                <directionalLight intensity={4} color="#fff" position={[0, 20, -20]} />
-                <directionalLight intensity={2} color="#fff" position={[20, 20, 0]} />
-                <directionalLight intensity={4} color="#fff" position={[0, 20, 20]} />
-                <directionalLight intensity={2} color="#fff" position={[-20, 20, 0]} />
                 <Model url={url} />
                 <OrbitControls
-                    target={targetPosition} // 设置观察目标的位置
-                    minDistance={2}    // 设置最小缩放距离
-                    maxDistance={10}   // 设置最大缩放距离
+                    target={targetPosition}
+                    minDistance={2}
+                    maxDistance={20}
                     minPolarAngle={0}
                     maxPolarAngle={Math.PI / 2}
-                    minAzimuthAngle={-Math.PI * (60 / 180)}
-                    maxAzimuthAngle={Math.PI * (60 / 180)}
+                    minAzimuthAngle={-Math.PI * (120 / 180)}
+                    maxAzimuthAngle={Math.PI * (120 / 180)}
                     rotateSpeed={0.3}
                 />
             </Canvas>
--- src/layout/layout-main/index.jsx
+++ src/layout/layout-main/index.jsx
@@ -2,12 +2,14 @@
  * @Author: JC96821 13478707150@163.com
  * @Date: 2023-09-02 20:25:59
  * @LastEditors: JC96821 13478707150@163.com
- * @LastEditTime: 2023-09-02 21:11:55
+ * @LastEditTime: 2023-09-03 21:55:30
  * @FilePath: \electron-react-template\src\layout\layout-main\index.jsx
  * @Description: 页面主体
  */
 
 import React, { Suspense } from 'react';
+
+import FallbackLoading from '@/components/fallback-loading';
 
 import routes from '../../configure/routes';
 import { renderRoutes } from '@/configure/routes/utils';
@@ -17,7 +19,7 @@ import styles from './index.module.less';
 const LayoutMain = () => {
     return (
         <div className={styles.contaner}>
-            <Suspense fallback="loading...">
+            <Suspense fallback={<FallbackLoading />}>
                 {renderRoutes(routes)}
             </Suspense>
         </div>
