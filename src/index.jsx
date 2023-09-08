c1669427ecc28ee56573dbb26f33d1ee584e9eff
WIN-J7OL7MK489U\EDY <13478707150@163.com>
2023-09-08 11:21:10 +0800

    [feature] add theme-colors config

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
  * @Author: JC96821 13478707150@163.com
  * @Date: 2023-09-02 16:37:44
  * @LastEditors: WIN-J7OL7MK489U\EDY 13478707150@163.com
- * @LastEditTime: 2023-09-08 10:47:29
+ * @LastEditTime: 2023-09-08 11:04:09
  * @FilePath: \electron-react-template\src\index.js
  * @Description: 客户端程序入口
  */
@@ -14,7 +14,7 @@ import PrimaryLayout from './layout/primary-layout';
 import { ConfigProvider as AntdConfigProvider, theme } from 'antd';
 import { initAppData } from './utils';
 
-import themeColors from '@/style/lessVariables';
+import themeColors from './style/themeColors';
 
 import './index.less';
 
---
+++ src/style/themeColors.js
@@ -0,0 +1,14 @@
+
+/*
+ * 'Author: JC 13478707150'163.com
+ * 'Date: 2022-12-26 14:41:15
+ * 'LastEditors: JC 13478707150'163.com
+ * 'LastEditTime: 2023-05-23 11:37:22
+ * 'FilePath: \digital-assets\src\style\lessVariables.js
+ * 'Description: 自定义less变量
+ */
+
+export default {
+    // 主题色
+    'colorPrimary': '#262626',
+};
