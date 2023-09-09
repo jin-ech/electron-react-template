b40ff7b638ddfcd7b910608b16095266489f462a
JC96821 <13478707150@163.com>
2023-09-09 18:50:58 +0800

    [feature] dashboard page => detail adjustment

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -12,6 +12,7 @@ import React from 'react';
 import { Button } from 'antd';
 
 import styles from './index.module.less';
+import { GithubOutlined } from '@ant-design/icons';
 
 const electron = window.require('electron');
 
@@ -19,6 +20,7 @@ const Dashboard = () => {
     
     const handleClick = () => {
         electron.ipcRenderer.send('notification', { message: 'Hello Electron React Template!' });
+        electron.shell.openExternal('https://github.com/jin-ech/electron-react-template');
     };
     
     const handleOpenDoc = () => {
@@ -29,7 +31,7 @@ const Dashboard = () => {
         <div className={styles.container}>
             <div className={styles.center}>
                 <div className={styles.logo}></div>
-                <Button type='primary' onClick={handleClick}>click here!</Button>
+                <Button type='primary' icon={<GithubOutlined />} onClick={handleClick}>repository</Button>
                 <a
                     className={styles.link}
                     onClick={handleOpenDoc}
