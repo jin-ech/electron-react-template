6789e7a46425e8aeee05b3600738283917af5d8e
JC96821 <13478707150@163.com>
2023-09-09 16:18:56 +0800

    [feature] add useMenuLayout

--- src/layout/layout-header/index.jsx
+++ src/layout/layout-header/index.jsx
@@ -2,52 +2,31 @@
  * @Author: JC96821 13478707150@163.com
  * @Date: 2023-09-02 20:25:53
  * @LastEditors: JC96821 13478707150@163.com
- * @LastEditTime: 2023-09-02 23:30:44
+ * @LastEditTime: 2023-09-09 16:18:35
  * @FilePath: \electron-react-template\src\layout\layout-header\index.jsx
  * @Description: 页面头部
  */
 
-import React, { useEffect, useMemo, useState } from 'react';
+import React from 'react';
 import cls from 'classnames';
 
-import { useLocation, useNavigate } from 'react-router-dom';
-import { Menu } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
-import layoutConfig from '@/configure/layout';
 
-import styles from './index.module.less';
 import useWindowResize from '@/hooks/useWindowResize';
 
+import styles from './index.module.less';
+
 const LayoutHeader = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [selectedKeys, updateSelectedKeys] = useState([]);
-
     const { isMaximized, onClose, onMinimize, onMaximize, onUnMaximize } = useWindowResize();
-
-    useEffect(() => {
-        updateSelectedKeys(location.pathname);
-    }, [location]);
-
-    const items = useMemo(() => layoutConfig.map(menu => ({
-        key: menu.key,
-        title: menu.title,
-        label: menu.title,
-        onClick: () => navigate(menu.path)
-    })), []);
 
     return (
         <div className={styles.container}>
-            <div className={styles.logo}></div>
-            <Menu
-                className={styles.menu}
-                style={{ background: 'transparent' }}
-                mode='horizontal'
-                selectedKeys={selectedKeys}
-                items={items}
-            />
+            <div className={styles.left}>
+                <div className={styles.logo}></div>
+                <div className={styles.title}> electron-react-template</div>
+            </div>
             <div className={styles.center}>
-                electron-react-template
+                {/* TODO */}
             </div>
             <div className={styles.right}>
                 <div className={cls(styles.icon, styles.minimize)} onClick={onMinimize}></div>
